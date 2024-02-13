7a9c289fe60401a3f948e4caaf74e863d7af167b
dary <749948071@qq.com>
2024-02-13 21:52:59 +0800

    update auto sdk

---
+++ Assets/Auto/AutoMato.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Matory;
+using System;
+
+namespace Mator
+{
+    public class AutoMato : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        static Mato runner = null;
+        private bool swich = false;
+        void Awake()
+        {
+            if (runner == null)
+            {
+                runner = gameObject.AddComponent<Mato>();
+                try
+                {
+                    if (runner != null)
+                    {
+                        runner.Init();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogException(ex);
+                }
+            }
+        }
+    }
+}
